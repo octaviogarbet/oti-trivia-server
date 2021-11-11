66b20cf665a007da58a4a6807fb0f57d9fa50404
Octavio Garbarino <octaviogarbarino@gmail.com>
2021-11-11 00:39:24 -0300

    WIP: Add basic endpoints

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors');
 const http = require("http")
 const app = express()
 const port = 3000
@@ -8,12 +9,133 @@ const WebSocket = require("ws")
 //initialize a simple http server
 const server = http.createServer(app);
 
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/game/:id', (req, res) => {
+  if (sessions.length > +req.params.id)
+    res.send(sessions[+req.params.id])
+  else
+    res.send({})
+})
+
+app.post('/join/:id', (req, res) => {
+  if (sessions.length > +req.params.id){
+    res.send(sessions[+req.params.id])
+  } else {
+    res.send({})
+  }
+})
+
+app.put('/game/:id/reset', (req, res) => {
+  if (sessions.length > +req.params.id){
+    sessions[+req.params.id].answerOrder = []
+    sessions[+req.params.id].banned = []
+    sessions[+req.params.id].answering = null
+    sessions[+req.params.id].answeringPosition = 0
+    sessions[+req.params.id].canAnswer = false
+    res.send({})
+  } else {
+    res.send({})
+  }
+})
+
+app.put('/game/:id/start', (req, res) => {
+  if (sessions.length > +req.params.id){
+    sessions[+req.params.id].canAnswer = true
+    res.send({})
+  } else {
+    res.send({})
+  }
+})
+
+app.put('/game/:id/wrong', (req, res) => {
+  if (sessions.length > +req.params.id){
+    if (sessions[+req.params.id].answerOrder.length === (sessions[+req.params.id].answeringPosition - 1)) {
+      sessions[+req.params.id].answeringPosition = 0;
+    } else {
+      sessions[+req.params.id].answeringPosition++;
+    }
+    sessions[+req.params.id].answering = sessions[+req.params.id].answerOrder[sessions[+req.params.id].answeringPosition];
+    res.send({})
+  } else {
+    res.send({})
+  }
+})
+
+app.put('/game/:id/correct', (req, res) => {
+  if (sessions.length > +req.params.id){
+    sessions[+req.params.id].answering
+
+    sessions[+req.params.id].nextQuestion
+    pointsTable
+    scoreBoard
+    nextQuestion.category
+    nextQuestion.points
+    res.send({})
+  } else {
+    res.send({})
+  }
+})
+
+app.put('/game/:id/skip', (req, res) => {
+  if (sessions.length > +req.params.id){
+    sessions[+req.params.id].answering
+
+    sessions[+req.params.id].nextQuestion
+    pointsTable
+    scoreBoard
+    nextQuestion.category
+    nextQuestion.points
+    res.send({})
+  } else {
+    res.send({})
+  }
+})
+
+app.put('/game/:id/answer', (req, res) => {
+  const { body } = req;
+  if (sessions.length > +req.params.id) {
+    sessions[+req.params.id].nextQuestion = { category: body.category, points: 25}
+    res.send({})
+  } else {
+    res.send({})
+  }
+})
+
+app.post('/game', (req, res) => {
+  const { body } = req;
+  const points = [];
+  for (let index = 1; index <= body.totalQuestions; index++) {
+    points.push(25 * index);
+  }
+  
+  const newSession = {
+    id: sessions.length,
+    players: [],
+    name: body.name,
+    canAnswer: false,
+    totalQuestions: body.totalQuestions,
+    totalCategories: body.totalCategories,
+    categories: body.categories,
+    answering: null,
+    answeringPosition: 0,
+    nextQuestion: null,
+    answerOrder: [],
+    banned: [],
+    scoreBoard: [],
+    pointsTable: body.categories.map(category => ({category, questions: points.map(p => ({points: p, answered: false}))}))
+  };
+  sessions.push(newSession);
+  console.log(req.body);
+
+  res.send(newSession)
 })
 
 //app.listen..
